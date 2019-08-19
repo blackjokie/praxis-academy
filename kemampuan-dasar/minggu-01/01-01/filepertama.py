6f440bbbd33a9e4a095164843eab49349e6adef0
010 <010@nappa.tech>
2019-08-19 14:06:55 +0700

    minggu 01, hari 01

---
+++ kemampuan-dasar/minggu-01/01-01/filepertama.py
@@ -0,0 +1,16 @@
+fname = "ibnu"
+lname = "harahap"
+age = 24
+
+print("Nama saya {0} {1}".format(fname, lname))
+print("Umur saya {0}".format(age))
+
+print("Nama saya " + fname + " " + lname)
+print(f"Nama saya {fname} {lname}")
+
+print("{0:.3f}".format(1.0/3))
+
+x = True
+y = False
+
+print (x != y)

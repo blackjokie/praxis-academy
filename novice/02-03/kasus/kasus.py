a0d41e8fd463624e000c2b362dfd67f6b1135054
010 <010@nappa.tech>
2019-08-28 13:19:12 +0700

    materi dan kasus minggu ke-2 pertemuan ke-3

---
+++ novice/02-03/kasus/kasus.py
@@ -0,0 +1,34 @@
+import argparse
+import concurrent.futures
+import requests
+import threading
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url", help="input url (ex: https://google.com)", type=str, required=True)
+args = vars(parser.parse_args())
+
+thread_local = threading.local()
+
+def get_session():
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+def download_site(url):
+    session = get_session()
+    with session.get(url) as response:
+        print(f"Read {len(response.content)} from {url}")
+
+def download_all_sites(sites):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_site, sites)
+
+if __name__ == "__main__":
+    sites = [
+        args["url"],
+    ] * 80
+    start_time = time.time()
+    download_all_sites(sites)
+    duration = time.time() - start_time
+    print(f"Downloaded {len(sites)} in {duration} seconds")
